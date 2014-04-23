6d5472bc98acadc56eb98bb55af800e22bf243c4
Ben Green <ben.green@inventati.org>
2014-04-23 19:37:56 -0400

    Added some skeleton code

--- myult.c
+++ myult.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <setjmp.h>
 #include "myult.h"
 
-int main(int argc, char const *argv[])
+int mypthread_create(mypthread_t *thread, void *(*function), void *arg)
 {
-	/* code */
-	return 0;
+
+}
+
+
+void mypthread_exit(void *returnvalue)
+{
+
+}
+
+void mypthread_join(mypthread_t thread, void **returnvalue)
+{
+
+}
+
+void mypthread_yield()
+{
+
 }
--- myult.h
+++ myult.h
@@ -1,3 +1,27 @@
+#ifndef MYULT
+#define MYULT
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <ucontext.h>
+
+
+#define STACK_SIZE 1024*128
+
+typedef struct mypthread
+{
+	typedef enum {READY, WAITING, KILLED, BLOCKED} state;
+} mypthread_t;
+
+typedef struct mymutex
+{
+	enum {LOCKED, UNLOCKED} state;
+	mypthread_t *owner
+} mymutex_t;
+
+
+
+
+
+
+#endif
